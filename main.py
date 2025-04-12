78a4f689348a768e8c717438700d816afa4a53f9
Sladjan999 <sladjan999@gmail.com>
2025-04-12 11:56:18 +0200

    Adding a class cart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 manager = ProductManager()
 
@@ -14,3 +15,12 @@ for product in products:
 
 manager.prikaz_svih_proizvoda()
 print(f"Ukupna vrednost svih proizvoda iznosi: {manager.ukupna_vrednost_svih_proizvoda()}")
+
+cart = Cart()
+
+cart.dodavanje_proizvoda_u_korpu(manager.products[0])
+cart.dodavanje_proizvoda_u_korpu(manager.products[1])
+cart.dodavanje_proizvoda_u_korpu(manager.products[2])
+
+cart.prikaz_sadrzaja_korpe()
+print(f"Ukupna vrednost korpe: {cart.ukupna_vrednost_korpe()}")
